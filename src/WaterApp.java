e6295dae88a95553cdb9cea441ed0c85e5d51103
marcsoftware <marcdavidmelcher@gmail.com>
2021-03-18 14:29:51 -0500

    added two textarea with split view, but need scroll bars

--- src/WaterApp.java
+++ src/WaterApp.java
@@ -1,65 +1,61 @@
 package src;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
- 
-/**
- * A Java Swing program that shows how much water you should drink a day.
- * @author www.codejava.net
- */
-public class WaterApp extends JFrame implements ActionListener {
-    private JLabel labelQuestion;
-    private JLabel labelWeight;
-    private JTextField fieldWeight;
-    private JButton buttonTellMe;
- 
-    public WaterApp() {
-        super("Water Calculator");
- 
-        initComponents();
- 
-        setSize(240, 150);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
- 
-    private void initComponents() {
-        labelQuestion = new JLabel("How much water should I drink?");
-        labelWeight = new JLabel("My weight (kg):");
-        fieldWeight = new JTextField(5);
-        buttonTellMe = new JButton("Tell Me");
- 
-        setLayout(new FlowLayout());
- 
-        add(labelQuestion);
-        add(labelWeight);
-        add(fieldWeight);
-        add(buttonTellMe);
- 
-        buttonTellMe.addActionListener(this);
-    }
- 
-    public void actionPerformed(ActionEvent event) {
-        String message = "Buddy, you should drink %.1f L of water a day!";
- 
-        float weight = Float.parseFloat(fieldWeight.getText());
-        float waterAmount = calculateWaterAmount(weight);
- 
-        message = String.format(message, waterAmount);
- 
-        JOptionPane.showMessageDialog(this, message);
-    }
- 
-    private float calculateWaterAmount(float weight) {
-        return (weight / 10f) * 0.4f;
-    }
- 
-    public static void main(String[] args) {
-       // new WaterApp().setVisible(true);
+// Java Program  to create a vertical  
+// JSplitPane to separate two text areas 
+import javax.swing.event.*; 
+import java.awt.*; 
+import javax.swing.*; 
+class WaterApp extends JFrame { 
+  
+    // frame 
+    static JFrame f; 
+  
+    // text areas 
+    static JTextArea t1, t2; 
+  
+    // main class 
+    public static void main(String[] args) 
+    { 
+        // create a new frame 
+        f = new JFrame("frame"); 
+
+  
+        // create a object 
+        WaterApp s = new WaterApp(); 
+  
+        // create a panel 
+        JPanel p1 = new JPanel(); 
+        JPanel p = new JPanel(); 
+  
+        // create text areas 
+        t1 = new JTextArea(10, 10); 
+        t2 = new JTextArea(10, 10); 
+  
+        // set texts 
+        t1.setText("this is first text area"); 
+        t2.setText("this is second text area"); 
+  
+        // add text area to panel 
+        p1.add(t1); 
+        p.add(t2); 
+  
+        // create a splitpane 
+        JSplitPane sl = new JSplitPane(SwingConstants.VERTICAL, p1, p); 
+  
+        // set Orientation for slider 
+        sl.setOrientation(SwingConstants.VERTICAL); 
+  
+        // add panel 
+        f.add(sl); 
+  
+        // set the size of frame 
+        f.setSize(300, 300); 
+    
+        f.show(); 
 
         
-        WaterApp myFrame = new WaterApp();
-        myFrame.setVisible(true);
-        myFrame.setExtendedState(myFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-    
-    }
-}
+         f.setVisible(true);
+    f.setExtendedState(f.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+
+
+    } 
+} 
