7f5012051bd4b9c2d306d0babc394479227f3b0a
marcsoftware <marcdavidmelcher@gmail.com>
2021-03-25 11:02:38 -0500

    hello.cmd works if hardcoded.java is in live/src

--- hello.cmd
+++ hello.cmd
@@ -1,5 +1,6 @@
-del /f live\src\*.class
-echo empty > output.txt
+cd live
+del /f src\*.class
+echo empty > ../output.txt
 
-javac -d . live/src/*.java 2> output.txt
-java -classpath . live.src.hardcoded >> output.txt
+javac -d . src/*.java 2> ../output.txt
+java -classpath . src.hardcoded >> ../output.txt
