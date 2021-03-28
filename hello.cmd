2483aa8cda456700064d868e5f8fee95b884e57a
marcsoftware <marcdavidmelcher@gmail.com>
2021-03-28 11:37:03 -0500

    FIXED HELLO.CMD -> any classname can be used in sample code

--- hello.cmd
+++ hello.cmd
@@ -2,5 +2,5 @@ cd live
 del /f src\*.class
 echo empty > ../output.txt
 
-javac -d . src/*.java 2> ../output.txt
-java -classpath . src.hardcoded >> ../output.txt
+javac -d . src/%1.java 2> ../output.txt
+java -classpath . src.%1 >> ../output.txt
